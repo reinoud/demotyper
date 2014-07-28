023cd936ba249d9f632a45116af701b16fcaa28c
Eric Williams <eric@subcritical.org>
2014-07-28 15:32:40 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# HackerTyper for Python
+
+hkrtpr is intended to provide [HackerTyper][hackertyper]-style productivity for any machine with a
+working python and curses implementation.
+
+
+[hackertyper]:http://hackertyper.com
+
---
+++ hackertyper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+import sys
+import curses
+import time
+
+import logging
+import random
+
+
+src = ""
+
+logging.basicConfig(filename="hackertyper.log", level=logging.DEBUG, 
+    format='%(asctime)s [%(levelname)s] %(message)s')
+
+def add_text(text_window, src_len):
+    
+    logging.debug("add_text entered")
+    # text_window.addstr(0,0,src[0:src_len])
+    contents = src[0:src_len]
+    # formatted_contents = highlight(contents, PythonLexer(), Terminal256Formatter())
+    text_window.addstr(0,0,contents)
+    
+
+def main(stdscr):
+    """
+    """
+    logging.debug("starting hackertyper")    
+    stdscr.clear()
+    src_len=0
+    while True and src_len < len(src):
+      add = random.choice(range(3))
+      src_len += add
+      input = stdscr.getkey()
+      logging.debug(input)
+      add_text(stdscr, src_len)    
+      
+if __name__=='__main__':
+    # load the file that should be used for the hackertyper
+    src = open(sys.argv[-1]).read()
+    curses.wrapper(main)    
+
