eb1706c58b7c9f8b3fb4a637a9749a900c49f86f
Reinoud van Leeuwen <rvanleeuwen@ebay.com>
2018-08-23 13:27:36 +0200

    PEP8

--- hackertyper.py
+++ hackertyper.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
-import os
 import sys
 import curses
-import time
 
 import logging
 import random
@@ -11,8 +9,8 @@ import random
 
 src = ""
 
-logging.basicConfig(filename="hackertyper.log", level=logging.DEBUG, 
-    format='%(asctime)s [%(levelname)s] %(message)s')
+logging.basicConfig(filename="hackertyper.log", level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
+
 
 def add_text(text_window, src_len):
     
@@ -20,7 +18,7 @@ def add_text(text_window, src_len):
     # text_window.addstr(0,0,src[0:src_len])
     contents = src[0:src_len]
     # formatted_contents = highlight(contents, PythonLexer(), Terminal256Formatter())
-    text_window.addstr(0,0,contents)
+    text_window.addstr(0, 0, contents)
     
 
 def main(stdscr):
@@ -28,15 +26,16 @@ def main(stdscr):
     """
     logging.debug("starting hackertyper")    
     stdscr.clear()
-    src_len=0
+    src_len = 0
     while True and src_len < len(src):
-      add = random.choice(range(3))
-      src_len += add
-      input = stdscr.getkey()
-      logging.debug(input)
-      add_text(stdscr, src_len)    
+        add = random.choice(range(3))
+        src_len += add
+        inputkey = stdscr.getkey()
+        logging.debug(inputkey)
+        add_text(stdscr, src_len)
       
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # load the file that should be used for the hackertyper
     src = open(sys.argv[-1]).read()
     curses.wrapper(main)    
